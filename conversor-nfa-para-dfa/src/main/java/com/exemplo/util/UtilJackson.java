67be0d8b58bc88bf173c5d9fd38743b3c0cdfc4c
gabrielrsnts <gabrielrodriguesec63@gmail.com>
2025-09-02 16:47:24 -0300

    "Criação dos métodos para ler e escrever arquivos JSON, utilizando biblioteca Jackson"

--- conversor-nfa-para-dfa/src/main/java/com/exemplo/util/UtilJackson.java
+++ conversor-nfa-para-dfa/src/main/java/com/exemplo/util/UtilJackson.java
@@ -1,5 +1,25 @@
 package com.exemplo.util;
 
+import com.exemplo.model.Nfa;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import java.io.File;
+import com.exemplo.model.Dfa;
+
 public class UtilJackson {
 
+    private static final ObjectMapper objectMapper = new ObjectMapper();
+
+    static {
+        // Configurar o mapper para formatar o JSON de forma legível
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
+
+    public static Nfa lerJsonNfa(String caminhoDoArquivo) throws Exception {
+        return objectMapper.readValue(new File(caminhoDoArquivo),Nfa.class);
+    }
+
+    public static void escreverJsonDfa(Dfa dfa, String caminhoDoArquivo) throws Exception {
+        objectMapper.writeValue(new File(caminhoDoArquivo), dfa);
+    }
 }
